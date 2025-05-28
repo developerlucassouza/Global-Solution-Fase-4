059bc08ffb23938f695cbc904f64227a5d5e086b
Lucas Souza <107756390+developerlucassouza@users.noreply.github.com>
2025-05-28 18:49:49 -0300

    update

---
+++ script.js
@@ -0,0 +1,14 @@
+function OndaDeCalor(t, u) {
+
+    let it = ((t - 30) / 5) * 2 + 1;
+    let iu = 3 - (u / 60) * 2;
+
+    let pesoT = 0.7;
+    let pesoU = 0.3;
+
+    let resultado = (pesoT * it + pesoU * iu) / (pesoT + pesoU);
+
+    if (resultado < 1.5) { return "Risco Baixo" } 
+    else if (resultado < 2.5) { return "Risco Moderado" }
+    else { return "Risco Alto" }
+}
