44bddb4f08a66da7c9e977a001a5b965c0141389
Lucas Souza <107756390+developerlucassouza@users.noreply.github.com>
2025-06-02 18:09:53 -0300

    up

--- script.js
+++ script.js
@@ -19,3 +19,11 @@ function Enchentes(p, t, i, h, d) {
     else if (resultado < 100) { return "Risco Moderado" }
     else { return "Risco Alto" }
 }
+
+function Sanitario(ca, as, ar, ph, pd) {
+    let resultado = (ca * ph) + (ar / (pd + 1) ) + (1 - as);
+
+    if (resultado < 1) { return "Risco Baixo" } 
+    else if (resultado < 2) { return "Risco Moderado" }
+    else { return "Risco Alto" }
+}
